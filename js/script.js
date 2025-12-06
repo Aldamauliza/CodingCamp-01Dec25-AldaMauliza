952090401933c6ae3b57ffb6b737350e6a07f221
Alda <maulizaalda@gmail.com>
2025-12-06 10:13:00 +0700

    perbaikan di html dan js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-/*==================== MENU ====================*/
+/* MENU */
 const navMenu = document.getElementById("nav-menu"),
     navToggle = document.getElementById("nav-toggle"),
     navClose = document.getElementById("nav-close");
 
-/*===== MENU SHOW =====*/
+/* MENU SHOW */
 if (navToggle) {
     navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu");
     });
 }
 
-/*===== MENU HIDDEN =====*/
+/* MENU HIDDEN */
 if (navClose) {
     navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu");
     });
 }
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/* REMOVE MENU MOBILE */
 const navLink = document.querySelectorAll(".nav__link");
 
 navLink.forEach((n) =>
@@ -26,7 +26,7 @@ navLink.forEach((n) =>
     })
 );
 
-/*==================== ACCORDION SKILLS ====================*/
+/* ACCORDION SKILLS */
 const skillsContent = document.getElementsByClassName("skills__content"),
     skillsHeader = document.querySelectorAll(".skills__header");
 
@@ -45,7 +45,7 @@ skillsHeader.forEach((el) => {
     el.addEventListener("click", toggleSkills);
 });
 
-/*==================== QUALIFICATION TABS ====================*/
+/* QUALIFICATION TABS */
 const tabs = document.querySelectorAll("[data-target]"),
     tabContents = document.querySelectorAll("[data-content]");
 
@@ -65,7 +65,7 @@ tabs.forEach((tab) => {
     });
 });
 
-/*==================== SERVICES MODAL ====================*/
+/* SERVICES MODAL */
 const modalViews = document.querySelectorAll(".services__modal"),
     modalBtns = document.querySelectorAll(".services__button"),
     modalCloses = document.querySelectorAll(".services__modal-close");
@@ -88,7 +88,7 @@ modalCloses.forEach((modalClose) => {
     });
 });
 
-/*==================== PORTFOLIO SWIPER  ====================*/
+/* PORTFOLIO SWIPER  */
 let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
@@ -103,7 +103,7 @@ let swiperPortfolio = new Swiper(".portfolio__container", {
     keyboard: true,
 });
 
-/*==================== TESTIMONIAL ====================*/
+/* TESTIMONIAL */
 let swiperTestimonial = new Swiper(".testimonial__container", {
     loop: true,
     grabCursor: true,
@@ -120,7 +120,7 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
     },
 });
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/* SCROLL SECTIONS ACTIVE LINK */
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
@@ -144,58 +144,46 @@ function scrollActive() {
 }
 window.addEventListener("scroll", scrollActive);
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/
+/* CHANGE BACKGROUND HEADER */
 function scrollHeader() {
     const nav = document.getElementById("header");
-    // When the scroll is greater than 200 viewport height, add the
-    // scroll-header class to the header tag
     if (this.scrollY >= 80) nav.classList.add("scroll-header");
     else nav.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
 
-/*==================== SHOW SCROLL UP ====================*/
+/* SHOW SCROLL UP */
 function scrollUp() {
     const scrollUp = document.getElementById("scroll-up");
-    // When the scroll is higher than 560 viewport height,
-    // add the show-scroll class to the a tag with the scroll-top class
     if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
     else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
 
-/*==================== DARK LIGHT THEME ====================*/
+/* DARK LIGHT THEME */
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "fa-sun";
 
-// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
 
-// We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
 const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "fa-moon" : "fa-sun");
 
-// We validate if the user previously chose a topic
 if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue
-    // was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
     themeButton.classList[selectedIcon === "fa-moon" ? "add" : "remove"](iconTheme);
 }
 
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme);
     themeButton.classList.toggle(iconTheme);
-    // We save the theme and the current icon that the user chose
     localStorage.setItem("selected-theme", getCurrentTheme());
     localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-/*========== Change theme start ==========*/
+/* Change theme start */
 const greenButton = document.querySelector(".green");
 greenButton.addEventListener("click", () => {
     document.body.classList.toggle("green");
@@ -208,4 +196,4 @@ const pinkButton = document.querySelector(".pink");
 pinkButton.addEventListener("click", () => {
     document.body.classList.toggle("pink");
 });
-/*========== Change theme end ==========*/
+/* Change theme end */
